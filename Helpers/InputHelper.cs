aaa87bb538efb44ad5321a8d5daf85f477dca128
johnLeavell <johnleavell@gmail.com>
2024-09-23 13:43:04 -0500

    added input helper for navigation

---
+++ Helpers/InputHelper.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace sqltest.Helpers
+{
+    public static class InputHelper
+    {
+        public static bool CheckForSpecialCommands(string input)
+        {
+            if (string.Equals(input, "menu", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else if (string.Equals(input, "exit", StringComparison.OrdinalIgnoreCase))
+            {
+                Environment.Exit(0);
+            }
+            return false;
+        }
+    }
+}
